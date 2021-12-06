09d5d8296647439edc166fec9255c22d4e8a7d4b
U0ffer <mmazko66@gmail.com>
2021-12-06 11:24:27 +0300

    feat: add ability to get path for output stream of syntax analyzer

--- MMA-2021/Parm.cpp
+++ MMA-2021/Parm.cpp
@@ -10,6 +10,7 @@ Parm::PARM Parm::getParm(int argc, char* argv[]) {
 		if (argument.substr(0, strlen(PARM_IN)).find(PARM_IN) != std::string::npos) result->in += argument.substr(strlen(PARM_IN));
 		else if (argument.substr(0, strlen(PARM_OUT)).find(PARM_OUT) != std::string::npos) result->out += argument.substr(strlen(PARM_OUT));
 		else if (argument.substr(0, strlen(PARM_LOG)).find(PARM_LOG) != std::string::npos) result->log += argument.substr(strlen(PARM_LOG));
+		else if (argument.substr(0, strlen(PARM_MFST)).find(PARM_MFST) != std::string::npos) result->mfst += argument.substr(strlen(PARM_MFST));
 	}
 	if (!result->in.empty()) {
 		if (result->in.length() > PARM_MAX_SIZE) throw ERROR_THROW(104);
@@ -20,6 +21,10 @@ Parm::PARM Parm::getParm(int argc, char* argv[]) {
 		if (result->log.empty()) {
 			result->log += result->in + PARM_LOG_DEFAULT_EXT;
 			if (result->log.length() > PARM_MAX_SIZE) throw ERROR_THROW(104);
+		}
+		if (result->mfst.empty()) {
+			result->mfst += result->in + PARM_MFST_DEFAULT_EXT;
+			if (result->mfst.length() > PARM_MAX_SIZE) throw ERROR_THROW(104);
 		}
 		return *result;
 	}
--- MMA-2021/Parm.h
+++ MMA-2021/Parm.h
@@ -3,10 +3,11 @@
 #define PARM_IN "-in:"
 #define PARM_OUT "-out:"
 #define PARM_LOG "-log:"
+#define PARM_MFST "-mfst:"
 #define PARM_MAX_SIZE 300
 #define PARM_OUT_DEFAULT_EXT ".out"
 #define PARM_LOG_DEFAULT_EXT ".log"
-
+#define PARM_MFST_DEFAULT_EXT ".mfst"
 
 namespace Parm
 {
@@ -15,6 +16,7 @@ namespace Parm
 		std::string in = "";
 		std::string out = "";
 		std::string log = "";
+		std::string mfst = "";
 	};
 
 	PARM getParm(int argc, char* argv[]);
